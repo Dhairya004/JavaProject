74f45f5f113868de7a842614b4848c3d001c5491
Dhairya004 <84791206+Dhairya004@users.noreply.github.com>
2023-03-09 13:02:55 +0530

    Add files via upload

---
+++ SubtractTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SubtractTwoNumbers {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a,b;
+    System.out.println("Enter the value of a and b");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    System.out.println("Sum:"+(a-b));
+  }
+}
